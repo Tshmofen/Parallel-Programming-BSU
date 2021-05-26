8f17d2c0afc4b503121943d1d17f837f627d7980
Timofey <timohatimafeikq@gmail.com>
2021-05-26 13:49:05 +0300

    Add duration time builder

---
+++ Projects/OptimalComposition/OptimalComposition/CompositionBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace OptimalComposition
+{
+    public class CompositionBuilder
+    {
+        private readonly List<int> _executionTimes;
+        private int _competingProcessorsAmount;
+        private int _identicalProcessorsAmount;
+        private int _overhead;
+
+        public CompositionBuilder(
+            List<int> executionTimes,
+            int competingProcessorsAmount,
+            int identicalProcessorsAmount,
+            int overhead
+            )
+        {
+            _executionTimes = executionTimes;
+            _competingProcessorsAmount = competingProcessorsAmount;
+            _identicalProcessorsAmount = identicalProcessorsAmount;
+            _overhead = overhead;
+        }
+
+        public List<int> BuildDurations()
+        {
+            var s = _executionTimes.Count;
+            
+            var durationsMatrix = new int[s + 1][];
+            for (var i = 0; i < durationsMatrix.Length; i++)
+                durationsMatrix[i] = new int[s + 1];
+            
+            for (var j = 1; j <= s; j++)
+            {
+                durationsMatrix[s][j] = _executionTimes[j - 1];
+            }
+            
+            for (var k = 1; s - k >= 2; k++)
+            {
+                for (var j = 1; j <= s - k; j++)
+                {
+                    durationsMatrix[s - k][j] = durationsMatrix[s - k + 1][j] + _executionTimes[j - 1 + k];
+                }
+            }
+
+            var maxTime = _executionTimes.Max();
+            var durations = durationsMatrix
+                .SelectMany(line => line)
+                .Where(duration => duration >= maxTime)
+                .Distinct()
+                .OrderBy(duration => duration)
+                .ToList();
+
+            return durations;
+        }
+    }
+}
---
+++ Projects/OptimalComposition/OptimalComposition/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OptimalComposition
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            var executionTimes = new List<int> {10, 20, 5, 20, 5, 10, 10, 5, 10, 5};
+            const int competingProcessorsAmount = 5;
+            const int identicalProcessorsAmount = 3;
+            const int overhead = 4;
+
+            var builder = new CompositionBuilder(
+                executionTimes, 
+                competingProcessorsAmount,
+                identicalProcessorsAmount,
+                overhead
+                );
+
+            foreach (var time in builder.BuildDurations())
+                Console.WriteLine(time);
+        }
+    }
+}
